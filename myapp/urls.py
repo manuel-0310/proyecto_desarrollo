6a07bf31c8e32ce2fe48ad4b52c1abcf560ce8ed
manuel-0310 <manucastil0310@gmail.com>
2025-05-08 22:19:54 -0500

    Update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,11 +1,11 @@
-
 from django.urls import path
-from .views import RegisterView, LoginView, ProfileView, authors
+from .views import authors, RegisterView, LoginView, ProfileView
 
 urlpatterns = [
+    path('authors/', authors, name='authors'),
     path('register/', RegisterView.as_view(), name='register'),
     path('login/',    LoginView.as_view(),    name='login'),
     path('profile/',  ProfileView.as_view(),  name='profile'),
-    path('authors/',  authors,                name='authors'),
 ]
+
 
